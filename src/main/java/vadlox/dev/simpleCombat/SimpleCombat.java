d8b69b7d06b41a964df642edfbdf4bf3076e0895
Vadlox <ramsesplayzytx@gmail.com>
2025-08-02 22:07:04 -0500

    Implement core plugin functionality with combat and scoreboard managers

--- src/main/java/vadlox/dev/simpleCombat/SimpleCombat.java
+++ src/main/java/vadlox/dev/simpleCombat/SimpleCombat.java
@@ -1,17 +1,49 @@
 package vadlox.dev.simpleCombat;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import vadlox.dev.simpleCombat.combat.CombatManager;
+import vadlox.dev.simpleCombat.listeners.CombatListener;
+import vadlox.dev.simpleCombat.listeners.CommandListener;
+import vadlox.dev.simpleCombat.scoreboard.ScoreboardManager;
 
 public final class SimpleCombat extends JavaPlugin {
+    private CombatManager combatManager;
+    private ScoreboardManager scoreboardManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Save default config if it doesn't exist
+        saveDefaultConfig();
 
+        // Initialize managers
+        combatManager = new CombatManager(this);
+        scoreboardManager = new ScoreboardManager(this);
+
+        // Register event listeners
+        getServer().getPluginManager().registerEvents(new CombatListener(this, combatManager), this);
+        getServer().getPluginManager().registerEvents(new CommandListener(this, combatManager), this);
+
+        getLogger().info("SimpleCombat has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Cleanup managers
+        if (combatManager != null) {
+            combatManager.shutdown();
+        }
+        if (scoreboardManager != null) {
+            scoreboardManager.shutdown();
+        }
+
+        getLogger().info("SimpleCombat has been disabled!");
+    }
+
+    public CombatManager getCombatManager() {
+        return combatManager;
+    }
+
+    public ScoreboardManager getScoreboardManager() {
+        return scoreboardManager;
     }
 }
