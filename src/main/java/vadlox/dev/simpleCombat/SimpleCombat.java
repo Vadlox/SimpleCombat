290dfc3c0acb6fe06747a995dd3f2a8c5a853de1
Vadlox <ramsesplayzytx@gmail.com>
2025-08-02 22:06:47 -0500

    Initial commit

---
+++ src/main/java/vadlox/dev/simpleCombat/SimpleCombat.java
@@ -0,0 +1,17 @@
+package vadlox.dev.simpleCombat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleCombat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
